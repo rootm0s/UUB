4cf87efbc92d311d6e8d64d442a76b1c4f83c8dd
rootm0s <rickardmalmkvist@hotmail.com>
2019-06-23 13:38:45 +0200

    🏗️

---
+++ build.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+import sys
+import py2exe
+
+try:
+	args = sys.argv[1]
+except Exception as error:
+	sys.exit()
+
+sys.argv.pop()
+sys.argv.append("py2exe")
+sys.argv.append("-q")
+
+# https://docs.python.org/3/library/platform.html#platform.architecture
+is_64bits = sys.maxsize > 2**32
+print("Building for Win{}...".format(64 if is_64bits else 32))
+
+opts = {
+	"py2exe": {
+		"compressed": 2,
+		"optimize": 2,
+		"bundle_files": 3 if is_64bits else 1,
+		#"dll_excludes": ["w9xpopen.exe"]
+	}
+}
+
+setup(console=[args], options=opts, zipfile=None)
